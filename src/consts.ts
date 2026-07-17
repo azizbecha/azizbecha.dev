d6a816bb726368a0222e9cf20e0e09808a9cf106
Aziz Becha <aziz07becha@gmail.com>
2026-07-17 18:24:54 +0100

    footer stuff

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,10 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const FOOTER_LINKS = [
+	{ label: 'github/', href: 'https://github.com/azizbecha', dir: true, external: true },
+	{ label: 'x/', href: 'https://x.com/azizbecha', dir: true, external: true },
+	{ label: 'linkedin/', href: 'https://linkedin.com/in/azizbecha', dir: true, external: true },
+	{ label: 'rss.xml', href: '/rss.xml', dir: false, external: false },
+];
