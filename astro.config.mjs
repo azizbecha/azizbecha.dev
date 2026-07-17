fee0e8ef4b1b81a6dac8258c9fc882bc5e2acc1f
Aziz Becha <aziz07becha@gmail.com>
2026-07-17 22:58:23 +0100

    added search

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,12 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, fontProviders } from 'astro/config';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://azizbecha.dev',
-	integrations: [mdx(), sitemap(), react()],
+	integrations: [mdx(), sitemap(), react(), pagefind()],
 	fonts: [
 		{
 			provider: fontProviders.local(),
